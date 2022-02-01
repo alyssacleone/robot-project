7f14a2f3a1ed35f8709410829839daa7ad98b9d4
RJ Yarwood <rjyarwood99@gmail.com>
2022-02-01 18:19:10 -0500

    added some comments

--- grid-space.py
+++ grid-space.py
@@ -1,4 +1,9 @@
+#This is the gridspace class
+#It will be the object you can check for information about location
 
+#Things you may want:
+    #If it has been visited
+    #if it contains a 'snack'
 class GridSpace:
 
     def __init__(self, visited, snack):
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #TODO create an algorithm that you want the robot the follow
 #PS That should go in a seperate file
+
+#Things you may want for an algorithm:
+    #Choose a space to move to and add it to the stack. repeat.
+    #If no more moves possible and not all snacks eaten pop the stack and try a different move
+    #If no valid move keep popping until there is
+    #When complete the stack will be the path taken
